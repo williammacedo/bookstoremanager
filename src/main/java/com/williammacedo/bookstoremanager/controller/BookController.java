deab1df9d4983ada7e1f8df3a5696fd7fe3521b2
William <macedo.franca@gmail.com>
2021-08-25 13:54:18 -0300

    Created .travis.yml file

--- src/main/java/com/williammacedo/bookstoremanager/controller/BookController.java
+++ src/main/java/com/williammacedo/bookstoremanager/controller/BookController.java
@@ -1,15 +1,25 @@
 package com.williammacedo.bookstoremanager.controller;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Api(tags = "BookController")
 @RestController
 @RequestMapping("/v1/books")
 public class BookController {
 
+    @ApiOperation("Return an example hello world")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Success method return")
+    })
     @GetMapping
-    public String hello() {
-        return "Hello World";
+    public ResponseEntity<String> hello() {
+        return ResponseEntity.ok( "Hello World");
     }
 }
---
+++ src/main/java/com/williammacedo/bookstoremanager/custominfoendpoint/InfoWebPointExtension.java
@@ -0,0 +1,32 @@
+package com.williammacedo.bookstoremanager.custominfoendpoint;
+
+import lombok.AllArgsConstructor;
+import org.springframework.boot.actuate.endpoint.annotation.ReadOperation;
+import org.springframework.boot.actuate.endpoint.web.WebEndpointResponse;
+import org.springframework.boot.actuate.endpoint.web.annotation.EndpointWebExtension;
+import org.springframework.boot.actuate.info.InfoEndpoint;
+import org.springframework.stereotype.Component;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@AllArgsConstructor
+@Component
+@EndpointWebExtension(endpoint = InfoEndpoint.class)
+public class InfoWebPointExtension {
+
+    private InfoEndpoint delegate;
+
+    @ReadOperation
+    public WebEndpointResponse<Map> info() {
+        Map<String, Object> info = this.delegate.info();
+        Integer status = getStatus(info);
+        Map<String, Object> customInfo = new HashMap<>(info);
+        customInfo.put("customInfo", "handsOn");
+        return new WebEndpointResponse<>(customInfo, status);
+    }
+
+    private Integer getStatus(Map<String, Object> info) {
+        return 200;
+    }
+}
