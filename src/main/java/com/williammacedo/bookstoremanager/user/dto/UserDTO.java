1f1aba74ab38b70596896074af39ffb5b85e099e
William <macedo.franca@gmail.com>
2021-09-04 21:51:24 -0300

    Annotations for validation added to UserDTO.

--- src/main/java/com/williammacedo/bookstoremanager/user/dto/UserDTO.java
+++ src/main/java/com/williammacedo/bookstoremanager/user/dto/UserDTO.java
@@ -1,10 +1,13 @@
 package com.williammacedo.bookstoremanager.user.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.williammacedo.bookstoremanager.user.enums.Gender;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.*;
 import java.time.LocalDate;
 
 @Data
@@ -12,11 +15,23 @@ import java.time.LocalDate;
 @NoArgsConstructor
 public class UserDTO {
     private Long id;
+    @NotBlank
+    @Size(max = 255)
     private String name;
+    @NotNull
+    @Max(120)
     private int age;
+    @NotNull
     private Gender gender;
+    @NotBlank
+    @Email
     private String email;
+    @NotBlank
     private String username;
+    @NotBlank
     private String password;
+    @NotNull
+    @ApiModelProperty(example = "01/01/1980")
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd/MM/yyyy")
     private LocalDate birthDate;
 }
